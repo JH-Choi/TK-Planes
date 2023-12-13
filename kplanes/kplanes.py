830c431dc6ac998f68fd526a963b9eea19bb9f29
c-maxey <christopher.j.maxey@gmail.com>
2023-12-12 20:10:38 -0500

    adjustments to rendering after training

--- kplanes/kplanes.py
+++ kplanes/kplanes.py
@@ -449,7 +449,11 @@ class KPlanesModel(Model):
                       ray_bundle.nears,
                       ray_bundle.fars]
         ray_stuffs = torch.concat(ray_stuffs,dim=-1)
-        ray_stuffs = ray_stuffs.reshape(-1,self.patch_size,self.patch_size,ray_stuffs.shape[-1]).permute(0,3,1,2)
+        if len(ray_stuffs.shape) == 2:
+            ray_stuffs = ray_stuffs.reshape(-1,self.patch_size,self.patch_size,ray_stuffs.shape[-1]).permute(0,3,1,2)
+        else:
+            ray_stuffs = ray_stuffs.unsqueeze(0).permute(0,3,1,2)
+
         for rbidx,ray_bundle in enumerate(ray_bundles[1:]):
             ray_stuffs = self.ray_bundle_encoder[rbidx](ray_stuffs)
 
@@ -491,7 +495,7 @@ class KPlanesModel(Model):
             if orig_shape is None:
                 rgb_image = rgb.reshape((-1,curr_dim,curr_dim,feat_dim)) #.transpose(2,0,1)
             else:
-                rgb_image = rgb.reshape(orig_shape + (feat_dim,))
+                rgb_image = rgb.reshape((1,) + orig_shape + (feat_dim,))
 
             rgb_image = rgb_image.permute(0,3,1,2)
             rgb_images.append(rgb_image)
